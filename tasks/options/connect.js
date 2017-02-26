27927c7600fcf42e91f82035f2459f800a896681
puneet negi <puneetnegi1986@gmail.com>
2017-02-26 03:25:45 -0800

    Update connect.js

--- tasks/options/connect.js
+++ tasks/options/connect.js
@@ -1,5 +1,4 @@
 var auth = require('../helpers/auth');
-var proxy = require('../helpers/proxy');
 
 var config = {
   /**
@@ -14,21 +13,6 @@ var config = {
     serverUrl: 'https://5c8cc0e1-9b1f-44e5-9d3e-06d878ac8960.predix-uaa.run.aws-usw02-pr.ice.predix.io',
     defaultClientRoute: '/about',
     base64ClientCredential: 'c2lsdmVyOnNpbHZlcjEyIw=='
-  },
-  /**
-   * --------- ADD YOUR SECURE ROUTES HERE ------------
-   *
-   * Please update the following object add your secure routes
-   *
-   * Note: Keep the /api in front of your services here to tell the proxy to add authorization headers.
-   * You'll need to update the url and instanceId.
-   */
-  proxy: {
-    '/api/view-service': {
-      url: 'https://predix-views.run.aws-usw02-pr.ice.predix.io',
-      instanceId: '0f2ef541-98e3-4298-bca4-e86c57699267',
-      pathRewrite: { '^/api/view-service': '/v1'}
-    }
   }
 };
 
@@ -41,9 +25,6 @@ module.exports = {
       hostname: 'localhost',
       middleware: function (connect, options) {
         var middlewares = [];
-
-        //add predix services proxy middlewares
-        middlewares = middlewares.concat(proxy.init(config.proxy));
 
         //add predix uaa authentication middlewaress
         middlewares = middlewares.concat(auth.init(config.uaa));
